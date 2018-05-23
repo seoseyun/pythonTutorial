1c1667d0fbe879c8293d68d627a6cfd7f62a20a7
seoseyun <fauoo1004@naver.com>
2018-05-23 17:59:27 +0900

    drawGraph

--- drawTutorial.py
+++ drawTutorial.py
@@ -1,21 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
+import sys
 
 #해당숫자별 각 그레이스케일 빈도 읽어들이기
 with open("./mnist/numCnt.json","r",encoding="utf-8") as fp:
     freq=json.load(fp)
-
+print(freq)
 num_dic = {}
 #각 숫자마다 계산하기
 for i in range(10):
-    num_dic[i] = [0 for n in range(256)]
-    for j in range(256) :
-        num_dic[i][j] = freq[i][j]
+    num_dic[i]=[]
+    num_dic[i] = freq[i]
 
-asclist = [[n for n in range(256)]]
+asclist = [[str(n) for n in range(256)]]
 df = pd.DataFrame(num_dic,index=asclist)
-
 plt.style.use('ggplot')
 df.plot(kind = "line")
 plt.savefig("test.png")
